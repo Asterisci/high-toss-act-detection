06df5c332e85879ac1b89728cfe979d0bef38b2c
asterisk <asterisk_fgf@outlook.com>
2021-07-18 17:02:18 +0800

    feat(save video): add save video

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ import cv2
 # 创建参数解析器并解析参数
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
-ap.add_argument("-a", "--min-area", type=int, default=1, help="minimum area size")
+ap.add_argument("-i", "--min-area", type=int, default=0, help="minimum area size")
+ap.add_argument("-x", "--max-area", type=int, default=10000, help="maximum area size")
 args = vars(ap.parse_args())
 
 # 如果video参数为None，那么我们从摄像头读取数据
@@ -18,7 +19,12 @@ if args.get("video", None) is None:
 
 # 否则我们读取一个视频文件
 else:
-    camera = cv2.VideoCapture(args["video"])
+    camera = cv2.VideoCapture(args['video'])
+    fps = camera.get(cv2.CAP_PROP_FPS)  # 帧数
+    width, height = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)), int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 宽高
+    print((width, height))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
 
 # 初始化视频流的第一帧
 firstFrame = None
@@ -36,9 +42,9 @@ while True:
         break
 
     # 调整该帧的大小，转换为灰阶图像并且对其进行高斯模糊
-    frame = imutils.resize(frame, width=500)
+    frame = imutils.resize(frame, width=width)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (17, 17), 0)
+    gray = cv2.GaussianBlur(gray, (15, 15), 0)
 
     # 如果第一帧是None，对其进行初始化
     if firstFrame is None:
@@ -52,7 +58,10 @@ while True:
     #     continue
 
     # 计算当前帧和第一帧的不同
-    frameDelta = cv2.absdiff(firstFrame, gray)
+    frameDelta1 = cv2.absdiff(firstFrame, secondFrame)
+    frameDelta2 = cv2.absdiff(secondFrame, gray)
+    frameDelta = cv2.bitwise_and(frameDelta1, frameDelta2)
+    # frameDelta = frameDelta * 10 + 100
     thresh = cv2.threshold(frameDelta, 10, 255, cv2.THRESH_BINARY)[1]
 
     # 扩展阀值图像填充孔洞，然后找到阀值图像上的轮廓
@@ -63,7 +72,7 @@ while True:
     # 遍历轮廓
     for c in cnts:
         # 遍历轮廓
-        if cv2.contourArea(c) < args["min_area"]:
+        if cv2.contourArea(c) < args["min_area"] or cv2.contourArea(c) > args["max_area"]:
             continue
 
         # 计算轮廓的边界框，在当前帧中画出该框，并且更新标签
@@ -71,18 +80,22 @@ while True:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         text = "Occupied"
 
-    # 在当前帧上写文字以及时间戳
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    # # 在当前帧上写文字以及时间戳
+    # cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+    #     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    # cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+    #     (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
+    # print(frame.shape)
+    out.write(frame)
     # 显示当前帧并记录用户是否按下按键
     cv2.imshow("Security Feed", frame)
     # cv2.imshow("gray", gray)
-    cv2.imshow("Thresh", thresh)
+    # cv2.imshow("Thresh", thresh)
     # cv2.imshow("Frame Delta", frameDelta)
     key = cv2.waitKey(1) & 0xFF
+
+
 
     # 如果q键被按下，跳出循环
     if key == ord("q"):
@@ -94,4 +107,5 @@ while True:
 
 # 清理摄像机资源并关闭打开的窗口
 camera.release()
+out.release()
 cv2.destroyAllWindows()
