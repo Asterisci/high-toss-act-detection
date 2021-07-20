797d8c64dbc56c2faf764843070cfdb480e2d5d7
asterisk <asterisk_fgf@outlook.com>
2021-07-20 21:21:19 +0800

    feat(backgroundsubtractor): add MOG MOG2 GMG KNN for BackgroundSubtractor

--- algorithm.py
+++ algorithm.py
@@ -89,7 +89,7 @@ def three_frame_diff(args, camera, out, T=10, blur=15):
     firstFrame = secondFrame
     secondFrame = gray
 
-def MOG(args, camera, out):
+def MOG(args, camera, out, T=10, blur=15):
   fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 
   while True:
@@ -98,7 +98,117 @@ def MOG(args, camera, out):
     if not grabbed:
       break
 
-    fgmask = fgbg.apply(frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (blur, blur), 0)
+
+    fgmask = fgbg.apply(gray)
+    thresh = cv2.threshold(fgmask, T, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in cnts:
+      if cv2.contourArea(c) < args["min_area"] or cv2.contourArea(c) > args["max_area"]:
+        continue
+
+      (x, y, w, h) = cv2.boundingRect(c)
+      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Security Feed", frame)
+    cv2.imshow("Mask", fgmask)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+      break
+
+def MOG2(args, camera, out, T=10, blur=15):
+  fgbg = cv2.createBackgroundSubtractorMOG2()
+
+  while True:
+    (grabbed, frame) = camera.read()
+
+    if not grabbed:
+      break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (blur, blur), 0)
+
+    fgmask = fgbg.apply(gray)
+    thresh = cv2.threshold(fgmask, T, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in cnts:
+      if cv2.contourArea(c) < args["min_area"] or cv2.contourArea(c) > args["max_area"]:
+        continue
+
+      (x, y, w, h) = cv2.boundingRect(c)
+      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Security Feed", frame)
+    cv2.imshow("Mask", fgmask)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+      break
+
+def GMG(args, camera, out, T=10, blur=15):
+  fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
+
+  while True:
+    (grabbed, frame) = camera.read()
+
+    if not grabbed:
+      break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (blur, blur), 0)
+
+    fgmask = fgbg.apply(gray)
+    thresh = cv2.threshold(fgmask, T, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in cnts:
+      if cv2.contourArea(c) < args["min_area"] or cv2.contourArea(c) > args["max_area"]:
+        continue
+
+      (x, y, w, h) = cv2.boundingRect(c)
+      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Security Feed", frame)
+    cv2.imshow("Mask", fgmask)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+      break
+
+def KNN(args, camera, out, T=10, blur=15):
+  fgbg = cv2.createBackgroundSubtractorKNN()
+
+  while True:
+    (grabbed, frame) = camera.read()
+
+    if not grabbed:
+      break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (blur, blur), 0)
+
+    fgmask = fgbg.apply(gray)
+    thresh = cv2.threshold(fgmask, T, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in cnts:
+      if cv2.contourArea(c) < args["min_area"] or cv2.contourArea(c) > args["max_area"]:
+        continue
+
+      (x, y, w, h) = cv2.boundingRect(c)
+      cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
     cv2.imshow("Security Feed", frame)
     cv2.imshow("Mask", fgmask)
 
