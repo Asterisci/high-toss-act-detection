d2fda7aa980e39625858856988275a208fe1dbd9
asterisk <asterisk_fgf@outlook.com>
2021-08-17 00:25:15 +0800

    feat(algorithm): add reverse_track

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,5 @@
 import cv2
+import _thread as thread
 import numpy as np
 from preprocess import add_blank
 
@@ -10,6 +11,7 @@ def diff_track(args, camera, out, T, blur, dis=100, angle=2):
   track_point = []
   frame_id = 0
   display_id = 1
+  img = []
 
   while True:
     (grabbed, frame) = camera.read()
@@ -17,9 +19,11 @@ def diff_track(args, camera, out, T, blur, dis=100, angle=2):
     if not grabbed:
       break
 
+    img.append(frame)
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     if args['blank']:
-      gray = add_blank(gray, args['blank'], 0) #添加时间水印条
+      gray = add_blank(gray, args['blank'], 0)  # 添加时间水印条
     gray = cv2.GaussianBlur(gray, (blur, blur), 0)
 
     fgmask = fgbg.apply(gray)
@@ -34,39 +38,46 @@ def diff_track(args, camera, out, T, blur, dis=100, angle=2):
 
       (x, y, w, h) = cv2.boundingRect(c)
       p = np.array([x, y])
-      
+
       if frame_id % 1 == 0:
 
         track_point_np = np.array(track_point)
         if track_point_np.size == 0:
-          track_point.append([x, y, w, h, frame_id, 0, 0, 0]) # [x, y, w, h, insert_frame_id, display_count, stop_count]
+          # [x, y, w, h, insert_frame_id, display_count, stop_count]
+          track_point.append([x, y, w, h, frame_id, 0, 0, 0])
           continue
-        
+
         track_dis = p - track_point_np[:, :2]
         _dis = np.sqrt(np.sum(np.square(track_dis), axis=1))
 
         if np.min(_dis) > dis:
-          track_point_np = np.vstack([track_point_np, [x, y, w, h, frame_id, 0, 0, 0]])
+          track_point_np = np.vstack(
+              [track_point_np, [x, y, w, h, frame_id, 0, 0, 0]])
           _index = track_point_np.shape[0] - 1
         else:
           _index = np.argmin(_dis)
           track = track_point_np[_index, :]
-          v = np.arctan2(track_dis[_index, 1], (track_dis[_index, 0])) * 180 / np.pi
+          v = np.arctan2(track_dis[_index, 1],
+                         (track_dis[_index, 0])) * 180 / np.pi
           if ((v > 60 or v < -30) and track_dis[_index, 1] > 0) or track_point_np[_index, 5] > 8:
             if track_point_np[_index, 5] > 5:
               if track_point_np[_index, 7] == 0:
                 track_point_np[_index, 7] = display_id
                 display_id += 1
                 _record_pos = [x, y, w, h, id]
-              cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-              cv2.putText(frame, str(track_point_np[_index, 7]), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (55,255,155), 2)
+              # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+              # cv2.putText(frame, str(track_point_np[_index, 7]), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (55,255,155), 2)
             track_point_np[_index, 5] += 1
-            track_point_np[_index, :4]= np.array([[x, y, w, h]])
+            track_point_np[_index, :4] = np.array([[x, y, w, h]])
 
         track_point_np[:, 6] += 1
         track_point_np[_index, 6] = 0
 
-        track_point_np = track_point_np[track_point_np[:, 6] < 10]
+        for point in track_point_np[np.logical_and(track_point_np[:, 6] >= 9, track_point_np[:, 5] > 5)]:
+          reverse_track(args, fgbg, img, point, str(point[7]), T, blur, dis)
+          # thread.start_new_thread( reverse_track, (img, ))
+
+        track_point_np = track_point_np[track_point_np[:, 6] < 9]
 
         track_point = track_point_np.tolist()
 
@@ -80,3 +91,88 @@ def diff_track(args, camera, out, T, blur, dis=100, angle=2):
       break
 
     frame_id += 1
+
+
+def reverse_track(args, fg, img, end_point, point_id, T, blur, dis=5):
+
+  track_point = end_point
+  reverse_count = 0
+
+  last = None
+
+  for rev_frame in reversed(img):
+
+    if reverse_count > 20:
+      print(track_point)
+      return track_point
+
+    gray = cv2.cvtColor(rev_frame, cv2.COLOR_BGR2GRAY)
+    if args['blank']:
+      gray = add_blank(gray, args['blank'], 0)  # 添加时间水印条
+    gray = cv2.GaussianBlur(gray, (blur, blur), 0)
+
+    (h, w) = gray.shape
+
+    if last is None:
+      last = gray
+      continue
+
+    fgmask_rev = fg.apply(gray)
+
+    y_0 = track_point[1]-50 if track_point[1]-50 > 0 else 0
+    y_1 = track_point[1]+track_point[3] + \
+        50 if track_point[1]+track_point[3]+50 < h else h
+    x_0 = track_point[0]-50 if track_point[0]-50 > 0 else 0
+    x_1 = track_point[0]+track_point[2] + \
+        50 if track_point[0]+track_point[2]+50 < w else w
+    fgmas_rev = fgmask_rev[y_0:y_1, x_0:x_1]
+
+    # print(y_0, y_1, x_0, x_1)
+
+    thresh = cv2.threshold(fgmas_rev, T, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(),
+                                 cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.imshow("mask", thresh)
+    key = cv2.waitKey(10) & 0xFF
+    detection_list = []
+
+    for c in cnts:
+      if cv2.contourArea(c) < args["min"] or cv2.contourArea(c) > args["max"]:
+        continue
+
+      (x, y, w, h) = cv2.boundingRect(c)
+      x = x - (50 if track_point[0]-50 > 0 else 0) + track_point[0]
+      y = y - (50 if track_point[1]-50 > 0 else 0) + track_point[1]
+      detection_list.append([x, y, w, h])
+
+    if len(detection_list) == 0:
+      reverse_count += 1
+      continue
+    else:
+      reverse_count = 0
+    detection_np = np.array(detection_list)
+    detection_dis = detection_np[:, :2]
+    detection_dis -= track_point[:2]
+    _dis = np.sqrt(np.sum(np.square(detection_dis), axis=1))
+
+    _index = np.argmin(_dis)
+    detection_dis += track_point[:2]
+    track_point = detection_np[_index, :]
+    v = np.arctan2(detection_dis[_index, 1],
+                   (detection_dis[_index, 0])) * 180 / np.pi
+
+    cv2.rectangle(rev_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(rev_frame, str(point_id), (x, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (55, 255, 155), 2)
+
+    cv2.imshow("Security Feed", rev_frame)
+    key = cv2.waitKey(10) & 0xFF
+
+    # if np.min(_dis) < 10:
+    #   reverse_count += 1
+    # else:
+    #   reverse_count = 0
+
+    last = gray
