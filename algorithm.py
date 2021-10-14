11f84fd53992f92e0e269d5df1e00899d3f8bfcb
asterisk <asterisk_fgf@outlook.com>
2021-10-14 23:34:18 +0800

    docs(annotation): add annotation

--- algorithm.py
+++ algorithm.py
@@ -7,100 +7,133 @@ from preprocess import add_blank
 
 from utils import *
 
-def diff_track(args, camera, T, blur, dis=100, angle=2):
+def diff_track(args, camera, T, blur):
+
+  # 背景检测方法
   fgbg = cv2.createBackgroundSubtractorKNN()
 
+  # 检测队列
   track_point = []
   frame_id = 0
   display_id = 1
   img = []
+  dis = args['dis']
+  anglemax = args['anglemax']
+  anglemin = args['anglemin']
+  count = args['count']
+  updatecount = args['updatecount']
+  reversedis = args['reversedis']
 
   while True:
+    # 获得帧
     (grabbed, frame) = camera.read()
     # print(frame_id)
 
+    # 如果没有帧就停止
     if not grabbed:
       break
 
+    # 回放序列
     img.append(frame)
 
+    # 灰度化
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 时间水印遮挡
     if args['blank']:
       gray = add_blank(gray, args['blank'], 0)
+
+    # 高斯模糊
     gray = cv2.GaussianBlur(gray, (blur, blur), 0)
 
+    # 背景检测
     fgmask = fgbg.apply(gray)
+
+    # 二值化
     thresh = cv2.threshold(fgmask, args['target'], 255, cv2.THRESH_BINARY)[1]
+
+    # 腐蚀膨胀
     thresh = cv2.dilate(thresh, None, iterations=2)
-    (cnts, _) = cv2.findContours(thresh.copy(),
-                                 cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 检测识别
+    (cnts, _) = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for c in cnts:
+      
+      # 判断检测框大小是否符合
       if cv2.contourArea(c) < args["min"] or cv2.contourArea(c) > args["max"]:
         continue
 
+      # 获得检测框位置信息
       (x, y, w, h) = cv2.boundingRect(c)
       p = np.array([x, y])
 
+      # 这个是方便改成三帧法留的接口
       if frame_id % 1 == 0:
 
+        # 如果没有候选框就直接添加
         track_point_np = np.array(track_point)
         if track_point_np.size == 0:
           track_point.append([x, y, w, h, frame_id, 0, 0, 0])
           continue
 
+        # 有候选框尝试匹配
+
+        # 计算位移/速度，由于时间恒定，所以这两个是一个概念
         track_dis = p - track_point_np[:, :2]
         _dis = np.sqrt(np.sum(np.square(track_dis), axis=1))
 
+        # 大于dis的直接变为新候选框
         if np.min(_dis) > dis:
           track_point_np = np.vstack(
               [track_point_np, [x, y, w, h, frame_id, 0, 0, 0]])
           _index = track_point_np.shape[0] - 1
+        
+        # 不小于dis判断是否能更新匹配到的候选框
         else:
           _index = np.argmin(_dis)
           track = track_point_np[_index, :]
-          v = np.arctan2(track_dis[_index, 1],
-                         (track_dis[_index, 0])) * 180 / np.pi
-          if ((v > 60 or v < -30) and track_dis[_index, 1] > 0) or track_point_np[_index, 5] > 8:
-            if track_point_np[_index, 5] > 5:
+          # 计算速度角
+          v = np.arctan2(track_dis[_index, 1],(track_dis[_index, 0])) * 180 / np.pi
+          # 更新规则
+          if ((v > anglemin or v < anglemax) and track_dis[_index, 1] > 0) or track_point_np[_index, 5] > 8:
+            # 识别到的次数大于count
+            if track_point_np[_index, 5] > count:
+              # 还没有记录过
               if track_point_np[_index, 7] == 0:
                 track_point_np[_index, 7] = display_id
                 display_id += 1
+                # 成为一个识别目标
                 _record_pos = [x, y, w, h, id]
-              # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-              # cv2.putText(frame, str(track_point_np[_index, 7]), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (55,255,155), 2)
+            
+            # 识别到的次数
             track_point_np[_index, 5] += 1
             track_point_np[_index, :4] = np.array([[x, y, w, h]])
 
+        # 没有被更新计数
         track_point_np[:, 6] += 1
+        # 更新的计数重置
         track_point_np[_index, 6] = 0
 
-        for point in track_point_np[np.logical_and(track_point_np[:, 6] >= 9, track_point_np[:, 5] > 5)]:
-          # reverse_track(args, frame_id, fgbg, img, point, str(point[7]), T, blur, dis)
-          t = threading.Thread(target=reverse_track, args=(args, frame_id, fgbg, img, point, str(point[7]), T, blur, dis))
+        # 9次没有更新进入且识别到的册数大于5回溯识别
+        for point in track_point_np[np.logical_and(track_point_np[:, 6] >= updatecount, track_point_np[:, 5] > count)]:
+          t = threading.Thread(target=reverse_track, args=(args, frame_id, fgbg, img, point, str(point[7]), T, blur, reversedis))
           t.start()
-          # thread.start_new_thread( reverse_track, (args, frame_id, fgbg, img, point, str(point[7]), T, blur, dis))
 
-        track_point_np = track_point_np[track_point_np[:, 6] < 9]
+        # 进入回溯识别的框从列表删除
+        track_point_np = track_point_np[track_point_np[:, 6] < updatecount]
 
         track_point = track_point_np.tolist()
 
+    # 如果回溯队列超长就出队
     if len(img) > args['img']:
       img.pop(0)
 
-    # out.write(frame)
-    # cv2.imshow("Security Feed", frame)
-    # cv2.imshow("Mask", gray)
-
-    # key = cv2.waitKey(1) & 0xFF
-
-    # if key == ord("q"):
-    #   break
-
+    # frame计数
     frame_id += 1
 
-
-def reverse_track(args, frame_id, fg, img, end_point, point_id, T, blur, dis=5):
+# 回溯识别
+def reverse_track(args, frame_id, fg, img, end_point, point_id, T, blur, dis):
 
   track_point = end_point
   reverse_count = 0
@@ -112,8 +145,10 @@ def reverse_track(args, frame_id, fg, img, end_point, point_id, T, blur, dis=5):
 
   for rev_frame in reversed(img):
 
+    # 位置数组
     pos_list.append(list(track_point[:4]))
 
+    # 检测空置时间
     if reverse_count > 20:
       break
 
@@ -131,6 +166,7 @@ def reverse_track(args, frame_id, fg, img, end_point, point_id, T, blur, dis=5):
 
     fgmask_rev = fg.apply(gray)
 
+    # 边界检测
     y_0 = track_point[1]-50 if track_point[1]-50 > 0 else 0
     y_1 = track_point[1]+track_point[3] + \
         50 if track_point[1]+track_point[3]+50 < h else h
@@ -139,15 +175,11 @@ def reverse_track(args, frame_id, fg, img, end_point, point_id, T, blur, dis=5):
         50 if track_point[0]+track_point[2]+50 < w else w
     fgmas_rev = fgmask_rev[y_0:y_1, x_0:x_1]
 
-    # print(y_0, y_1, x_0, x_1)
 
     thresh = cv2.threshold(fgmas_rev, T, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
-    (cnts, _) = cv2.findContours(thresh.copy(),
-                                 cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (cnts, _) = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # cv2.imshow("mask", thresh)
-    # key = cv2.waitKey(10) & 0xFF
     detection_list = []
 
     for c in cnts:
@@ -175,22 +207,13 @@ def reverse_track(args, frame_id, fg, img, end_point, point_id, T, blur, dis=5):
     v = np.arctan2(detection_dis[_index, 1],
                    (detection_dis[_index, 0])) * 180 / np.pi
 
+    # 添加检测框与ID
     cv2.rectangle(rev_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.putText(rev_frame, str(point_id), (x, y),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (55, 255, 155), 2)
 
-    # out = args['out']
-    # out.write(rev_frame)
-
+    # 检测结果视频序列
     frame_list.append(rev_frame)
-
-    # cv2.imshow("Security Feed", rev_frame)
-    # key = cv2.waitKey(10) & 0xFF
-
-    # if np.min(_dis) < 10:
-    #   reverse_count += 1
-    # else:
-    #   reverse_count = 0
 
     last = gray
     frame_id -= 1
--- main.py
+++ main.py
@@ -9,11 +9,11 @@ import threading
 from algorithm import *
 
 def detection(args):
-
-  print(args['video'])
   urls = args['video'].split(' ')
 
   for url in urls:
+
+    # 获得视频参数
     print(url)
     camera = cv2.VideoCapture(url)
     fps = camera.get(cv2.CAP_PROP_FPS)
@@ -21,19 +21,25 @@ def detection(args):
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     args['config'] = [fourcc, fps, (width, height)]
 
+    # 多线程调用实现多路同时识别
     t = threading.Thread(target=diff_track, args=(args, camera, args['target'], args['blur']))
     t.start()
 
-  camera.release()
-
+# 获得配置
 if __name__ == "__main__":
   ap = argparse.ArgumentParser()
-  ap.add_argument("--video", type=str, default='', help="video rstp link")
-  ap.add_argument("--min", type=int, default=0, help="minimum area size")
-  ap.add_argument("--max", type=int, default=500, help="maximum area size")
-  ap.add_argument("--blur", type=int, default=5, help="blur")
-  ap.add_argument("--target", type=int, default=5, help="target")
-  ap.add_argument("--blank", nargs='+', type=int)
-  ap.add_argument("--img", type=int, default=300, help="playback img size")
+  ap.add_argument("--video", type=str, default='', help="video rstp link") # 流地址
+  ap.add_argument("--min", type=int, default=0, help="minimum area size") # 最小检测框
+  ap.add_argument("--max", type=int, default=500, help="maximum area size") # 最大检测框
+  ap.add_argument("--blur", type=int, default=5, help="blur") # 高斯模糊范围
+  ap.add_argument("--target", type=int, default=5, help="target") # 二值化阈值
+  ap.add_argument("--blank", nargs='+', type=int) # 时间水印遮挡框
+  ap.add_argument("--img", type=int, default=300, help="playback img size") # 回放序列保存长度
+  ap.add_argument("--dis", type=int, default=100, help="track point combine distance") # 候选框能到匹配的距离
+  ap.add_argument("--anglemax", type=int, default=-30, help="max angle") # 小于此角度
+  ap.add_argument("--anglemin", type=int, default=60, help="min angle") # 大于此角度
+  ap.add_argument("--count", type=int, default=5, help="detection count") # 识别到的次数
+  ap.add_argument("--updatecount", type=int, default=9, help="detection count no longer update count") # 识别到框没有再更新计数
+  ap.add_argument("--reversedis", type=int, default=100, help="reverse dis") # 回溯检测检测框大小
   args = vars(ap.parse_args())
   detection(args)
